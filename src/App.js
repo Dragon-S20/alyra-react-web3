0e3aceaa197a7d90c8f27d80f8743fdbd05bd573
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2020-11-04 09:59:17 -0500

    provider, account and balance added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Heading, VStack, Text, Center} from '@chakra-ui/core'
 import { useEffect, useReducer } from 'react'
 
 /* https://docs.ethers.io/v5/ */
-/*import { ethers } from 'ethers'*/
+import { ethers } from 'ethers'
 
 
 const web3Reducer = (state, action) => {
@@ -12,6 +12,14 @@ const web3Reducer = (state, action) => {
       return { ...state, isWeb3: action.isWeb3 }
       case 'SET_enabled':
         return { ...state, isEnabled: action.isEnabled}
+        case 'SET_account' : 
+        return{ ...state, account: action.account}
+        case 'SET_provider': 
+        return { ...state, provider: action.provider}
+        case 'SET_network': 
+        return {...state, network:action.network}
+        case 'SET_balance':
+          return {...state, balance: action.balance}
     default:
       throw new Error (`Unhandled action ${action.type} in webreducer`)
   }
@@ -23,7 +31,7 @@ const initialWeb3State =  {
   isWeb3: false,
   isEnabled: false,
 //is metamask : false
-  account: '0x0',
+  account: ethers.constants.AddressZero,
   //probider c'est la connection a la blockchain
   provider: null,
   //C'est une information de lecture
@@ -55,6 +63,7 @@ function App() {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'
     })
     dispatch({type: 'SET_enabled', isEnabled: true})
+    dispatch({ type: 'SET_account', account: accounts[0]})
   
   } catch(e) {
     console.log('Error:', e)
@@ -66,9 +75,26 @@ function App() {
     
   }, [state.isWeb3])
 
-  useEffect(() =>{
+  //connect to provider and get information
 
-  }, [state.isEnabled])
+  useEffect(() => {
+    const connect2Provider = async () => {
+      try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    dispatch({ type: 'SET_provider', provider: provider })
+    const network = await provider.getNetwork()
+    dispatch({ type: 'SET_network', network: network})
+    const _balance = await provider.getBalance(state.account)
+    const balance = ethers.utils.formatEther(_balance)
+    dispatch({ type: 'SET_balance', balance: balance})
+  } catch(e){
+    dispatch({type: 'SET_network', network: initialWeb3State.network})
+    dispatch({type : 'SET_balance', balance: initialWeb3State.balance})
+
+  }}
+   if (state.isEnabled && state.account !== ethers.constants.AddressZero){
+  connect2Provider ()}
+}, [state.isEnabled, state.account])
 
   return (
     <>
@@ -76,13 +102,17 @@ function App() {
         <Heading mb={10}>Web 3 demo</Heading>
         </Center>
         <VStack>
-  <Text>Web 3 environnement: {state.isWeb3 ? 'Injected' : 'uninjected'}</Text>
+        <Text>Web 3 environnement: {state.isWeb3 ? 'Injected' : 'uninjected'}</Text>
         <Text>Metamask status: {state.isEnabled ? 'Connected' : 'disconnected'} </Text>
-        <Text>Account:{initialWeb3State.account} </Text>
-        <Text>Balance: {initialWeb3State.balance}  </Text>
-        <Text>Nework: </Text>
+        {state.isEnabled && <Text>Account:{state.account} </Text>}
+        <Text>balance: {state.balance}</Text>
+        {state.netword && (
+        <>
+        <Text>network name: {state.network.name} </Text>
+        <Text>Nework id : {state.network.chainId} </Text>
+        </>
+        )}
       </VStack>
-      
     </>
   );
 }
